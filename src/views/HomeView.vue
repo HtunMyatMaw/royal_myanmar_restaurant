a96fa7e279acb10f043835332786b26c024bd94b
KKMSA <kokoaung6689195@gmail.com>
2024-05-06 18:32:55 +0900

    create home page source

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,88 @@
 <template>
   <main>
-      <h2>Home Menu</h2>
+    <div class="container">
+      <div class="content-div">
+          <h2>Warmly Welcome to Royal Myanmar Restaurant</h2>
+          <p>You can enjoy myanamar tastes that you've never tried.And we won't never let you down!</p>
+          <!-- <img src="../assets/img/inle-leg-rower.png" class="img-fluid" alt="Inle Leg Rower"/> -->
+      </div>
+      
+      <!-- Appetizers -->
+      <div class="row">
+        <h2>Appetizers</h2>
+        <hr>
+        <div class="col-md-3 col-xs-12">
+          <div class="cuisine-div">
+            <img src="../assets/img/coconut-noodle.png" class="img-thumbnail img-fluid" alt="Coconut Noodle"/>
+            <h2>Cuisine Name</h2>
+            <h5>price</h5>
+            <p>Description/Ingredients</p>
+          </div>
+        </div>
+        <div class="col-md-3 col-xs-12">
+          <div class="cuisine-div">
+            <img src="../assets/img/coconut-noodle.png" class="img-thumbnail img-fluid" alt="Coconut Noodle"/>
+            <h2>Cuisine Name</h2>
+            <h5>price</h5>
+            <p>Description/Ingredients</p>
+          </div>
+        </div>
+        <div class="col-md-3 col-xs-12">
+          <div class="cuisine-div">
+            <img src="../assets/img/coconut-noodle.png" class="img-thumbnail img-fluid" alt="Coconut Noodle"/>
+            <h2>Cuisine Name</h2>
+            <h5>price</h5>
+            <p>Description/Ingredients</p>
+          </div>
+        </div>
+        <div class="col-md-3 col-xs-12">
+          <div class="cuisine-div">
+            <img src="../assets/img/coconut-noodle.png" class="img-thumbnail img-fluid" alt="Coconut Noodle"/>
+            <h2>Cuisine Name</h2>
+            <h5>price</h5>
+            <p>Description/Ingredients</p>
+          </div>
+        </div>
+      </div>
+
+      <!-- Curry Collection -->
+      <div class="row">
+        <h2>Curry Collection</h2>
+        <hr>
+        <div class="col-md-3 col-xs-12">
+          <div class="cuisine-div">
+            <img src="../assets/img/coconut-noodle.png" class="img-thumbnail img-fluid" alt="Coconut Noodle"/>
+            <h2>Cuisine Name</h2>
+            <h5>price</h5>
+            <p>Description/Ingredients</p>
+          </div>
+        </div>
+        <div class="col-md-3 col-xs-12">
+          <div class="cuisine-div">
+            <img src="../assets/img/coconut-noodle.png" class="img-thumbnail img-fluid" alt="Coconut Noodle"/>
+            <h2>Cuisine Name</h2>
+            <h5>price</h5>
+            <p>Description/Ingredients</p>
+          </div>
+        </div>
+        <div class="col-md-3 col-xs-12">
+          <div class="cuisine-div">
+            <img src="../assets/img/coconut-noodle.png" class="img-thumbnail img-fluid" alt="Coconut Noodle"/>
+            <h2>Cuisine Name</h2>
+            <h5>price</h5>
+            <p>Description/Ingredients</p>
+          </div>
+        </div>
+        <div class="col-md-3 col-xs-12">
+          <div class="cuisine-div">
+            <img src="../assets/img/coconut-noodle.png" class="img-thumbnail img-fluid" alt="Coconut Noodle"/>
+            <h2>Cuisine Name</h2>
+            <h5>price</h5>
+            <p>Description/Ingredients</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -9,5 +91,15 @@
 </script>
 
 <style scoped>
-
+.content-div {
+  background:url("../assets/img/inle-leg-rower.png");
+  background-size: cover;
+  height: 400px;
+  background-position: top center;
+}
+.cuisine-div {
+    background: #f8f8f8;
+    box-shadow: 0px 1px 1px rgba(0,0,0,0.5);
+    margin: 10px 0px;
+}
 </style>
